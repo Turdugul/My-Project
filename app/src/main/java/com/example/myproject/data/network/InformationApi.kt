4c25b46d2fe9e94c2e8cf46fbba20126f860e9ec
Turdugul <okonbaevaturdugul@gmail.com>
2022-06-10 16:35:58 +0100

    int

--- app/src/main/java/com/example/myproject/data/network/InformationApi.kt
+++ app/src/main/java/com/example/myproject/data/network/InformationApi.kt
@@ -4,10 +4,12 @@ import com.example.myproject.data.models.InformationDto
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface InformationApi {
     @GET("api/data/myproject")
     fun getInformation(
+        @Query("pageSize") size: Int = 41
     ): Single<List<InformationDto>>
     @GET("api/data/myproject/{objectId}")
     fun getInfoById(@Path("objectId") objectId: String): Single<InformationDto>
